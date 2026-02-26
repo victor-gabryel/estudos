606d421ea63f86d1edff2349658bfec3743fa22b
Victor Gabryel da Silva <159219999+victor-gabryel@users.noreply.github.com>
2026-02-26 10:46:30 -0300

    version 03

---
+++ NodeJS/Aula04/Test/calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./somar");
+var SubFunc = require("./sub");
+var MultiFunc = require("./multi");
+var DivFunc = require("./div");
+
+console.log(`${SomaFunc(1,2)}`);
+console.log(`${SubFunc(10,5)}`);
+console.log(`${MultiFunc(100,10)}`);
+console.log(`${DivFunc(30,5)}`);
